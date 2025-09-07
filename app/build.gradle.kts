417aed8b5edd679af6a81d12d3c62f515cc36908
issaclsakee <issac@delightlabs.io>
2025-09-07 14:30:28 +0900

    feat: basic setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":biometricslib"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.biometric)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "biometrics"
 include(":app")
+include(":biometricslib")
